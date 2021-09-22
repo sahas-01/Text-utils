a4620439fc30b28ba37330a7e93e4a911498c8a9
Sahas <sahasvivek001@gmail.com>
2021-09-22 20:10:54 +0530

    minor change-added comp

--- src/App.js
+++ src/App.js
@@ -5,7 +5,11 @@ import Navbar from './Components/Navbar';
 function App() {
   return (
     <>
-      <Navbar text="TextUtils"/>
+      <Navbar text="TextUtils" />
+      <div class="mb-3">
+        <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+      </div>
     </>
   );
 }
